9b49d6f41b247154f8e625ecb2c86269afc5e41f
Ezedin Aliyi <ezedinaliyi38@gmail.com>
2026-02-02 16:22:20 +0300

    Add backend/models/Student.js

---
+++ backend/models/Student.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema({
+    studentId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['M', 'F'],
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    room: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Room'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
